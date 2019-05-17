4d6fb6f1aa3ea451628b350dba1a86b8fed3b5aa
873467017 <32980779+873467017@users.noreply.github.com>
2019-05-17 12:57:02 +0100

    Add files via upload

--- Facility/functions.php
+++ Facility/functions.php
@@ -1,4 +1,5 @@
 <?php
+
   function generateNewString($len=10){
     $token="fjslgjfJALKNLJKFDNL129";
     $token=str_shuffle($token);
@@ -11,5 +12,165 @@
     header('Location:login.php');
     exit();
   }
-  
+
+  function sqltodataEvent($sql){
+
+    require"conn.php";
+    $query=$conn->query($sql);
+    $num_rows=$query->rowCount();
+    if($num_rows>0){
+      echo "<table style='border: solid 1px black;'>";
+      echo "<tr><th>Name</th><th>Start Time</th><th>End Time</th></tr>";
+
+      class TableRows extends RecursiveIteratorIterator {
+      function __construct($it) {
+          parent::__construct($it, self::LEAVES_ONLY);
+      }
+
+      function current() {
+          return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+      }
+
+      function beginChildren() {
+          echo "<tr>";
+      }
+
+      function endChildren() {
+          echo "</tr>" . "\n";
+      }
+  }
+      $stmt=$conn->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+       echo $v;}
+
+       echo "</table>";
+
+
+    }else {
+      echo "Your search query does not match any data!";
+    }
+  }
+
+  function sqltodataCourse($sql){
+
+    require"conn.php";
+    $query=$conn->query($sql);
+    $num_rows=$query->rowCount();
+    if($num_rows>0){
+      echo "<table style='border: solid 1px black;'>";
+      echo "<tr><th>Name</th><th>Weeks</th></tr>";
+
+      class TableRows extends RecursiveIteratorIterator {
+      function __construct($it) {
+          parent::__construct($it, self::LEAVES_ONLY);
+      }
+
+      function current() {
+          return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+      }
+
+      function beginChildren() {
+          echo "<tr>";
+      }
+
+      function endChildren() {
+          echo "</tr>" . "\n";
+      }
+  }
+      $stmt=$conn->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+       echo $v;}
+
+       echo "</table>";
+
+
+    }else {
+      echo "Your search query does not match any data!";
+    }
+  }
+
+
+  function sqltodataFacility($sql){
+
+    require"conn.php";
+    $query=$conn->query($sql);
+    $num_rows=$query->rowCount();
+    if($num_rows>0){
+      echo "<table style='border: solid 1px black;'>";
+      echo "<tr><th>Name</th><th>Weeks</th></tr>";
+
+      class TableRows extends RecursiveIteratorIterator {
+      function __construct($it) {
+          parent::__construct($it, self::LEAVES_ONLY);
+      }
+
+      function current() {
+          return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+      }
+
+      function beginChildren() {
+          echo "<tr>";
+      }
+
+      function endChildren() {
+          echo "</tr>" . "\n";
+      }
+  }
+      $stmt=$conn->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+       echo $v;}
+
+       echo "</table>";
+
+
+    }else {
+      echo "Your search query does not match any data!";
+    }
+  }
+
+  function sqltodataContent($sql){
+
+    require"conn.php";
+    $query=$conn->query($sql);
+    $num_rows=$query->rowCount();
+    if($num_rows>0){
+      echo "<table style='border: solid 1px black;'>";
+      echo "<tr><th>Name</th><th>Weeks</th></tr>";
+
+      class TableRows extends RecursiveIteratorIterator {
+      function __construct($it) {
+          parent::__construct($it, self::LEAVES_ONLY);
+      }
+
+      function current() {
+          return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+      }
+
+      function beginChildren() {
+          echo "<tr>";
+      }
+
+      function endChildren() {
+          echo "</tr>" . "\n";
+      }
+  }
+      $stmt=$conn->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+       echo $v;}
+
+       echo "</table>";
+
+
+    }else {
+      echo "Your search query does not match any data!";
+    }
+  }
  ?>
