7e085377a0480e007063c42d1e2285bc3a7d3d94
zhangqing7 <50427845+zhangqing7@users.noreply.github.com>
2019-05-18 17:55:25 +0100

    part of admin
    version1

---
+++ DUS-admin/js/validate.js
@@ -0,0 +1,78 @@
+//check field whether is empty
+function validate_required(field, alerttxt) {
+    with (field) {
+        if (value == null || value == "") {
+            alert(alerttxt);
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+
+//return msg to user when these situation occur
+function validate_form(thisform) {
+    with (thisform) {
+
+        if (validate_required(name, "course name must be filled out!") == false) {
+            name.focus();
+            return false
+        } else if (validate_required(time, "course time must be filled out!") == false) {
+            time.focus();
+            return false
+        } else if (validate_required(weeks, "course duration must be filled out!") == false) {
+            weeks.focus();
+            return false
+        } else if (validate_required(start, "course start time must be filled out!") == false) {
+            start.focus();
+            return false
+        }
+
+    }
+}
+
+function validate_form_facility(thisform) {
+    with (thisform) {
+
+        if (validate_required(name, "facility name must be filled out!") == false) {
+            name.focus();
+            return false
+        // } else if (validate_required(img, "facility image must be uploaded!") == false) {
+        //     facility.focus();
+        //     return false
+         } else if (validate_required(price, "facility price must be filled out!") == false) {
+            price.focus();
+            return false
+        } else if (validate_required(capability, "facility capability must be filled out!") == false) {
+            capability.focus();
+            return false
+        } else if (validate_required(open, "facility open used time must be filled out!") == false) {
+            open.focus();
+            return false
+        } else if (validate_required(close, "facility close used time must be filled out!") == false) {
+            close.focus();
+            return false
+        } else if (validate_required(info, "facility info used time must be filled out!") == false) {
+            info.focus();
+            return false
+        }
+    }
+}
+
+function validate_form_news(thisform) {
+    with (thisform) {
+
+        if (validate_required(title, " news title must be filled out!") == false) {
+            title.focus();
+            return false
+        } else if (validate_required(time, "news time must be filled out!") == false) {
+            time.focus();
+            return false
+        } else if (validate_required(content, "news content duration must be filled out!") == false) {
+            content.focus();
+            return false
+        }
+
+    }
+}
