2ddd93bb9769d673415a37b31bb9633d21a7980e
zihanZhao1 <50427581+zihanZhao1@users.noreply.github.com>
2019-05-19 10:53:15 +0800

    admin_v2

---
+++ DUS_admin_v2/php/database.php
@@ -0,0 +1,15 @@
+<?php
+$db_host = 'mysql:host=localhost';
+$db_name = 'dbname=test';
+$db_user = 'root';
+$db_pass = '';
+try{
+    $pdo = new PDO($db_host.';'.$db_name, $db_user, $db_pass);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
