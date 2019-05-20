94d49441c896b00c4eecef2d4a10d74f357aa5b9
zhangqing7 <50427845+zhangqing7@users.noreply.github.com>
2019-05-20 19:24:39 +0100

    Add files via upload

---
+++ DUS_adminV4/php/foot.php
@@ -0,0 +1,53 @@
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Our Facilities - Durham University</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="../css/team-durham.css" type="text/css">
+    <link rel="stylesheet" href="../css/index.css" type="text/css">
+</head>
+<body>
+<div class="container-fluid no-border">
+    <div id="footer" class="row-fluid">
+        <div class="span12">
+            <ul class="nav nav-pills">
+                <li><a href='https://www.dur.ac.uk/contactform2/?pageid=59579'>Comments &amp; Questions</a></li>
+                <li><a href='https://www.dur.ac.uk/about/terms/'>Disclaimer</a></li>
+                <li><a href='https://www.dur.ac.uk/about/trading_name/'>Trading Name</a></li>
+                <li><a href="https://www.dur.ac.uk/about/cookies/">Cookie usage policy</a></li>
+                <li><a href="https://www.dur.ac.uk/ig/dp/privacy/">Privacy Notices</a></li>
+                <li class="status">Team Durham is part of Durham University. &nbsp; Updated: 10th May 2019</li>
+            </ul>
+
+            <script type="text/javascript">
+                var _gaq = _gaq || [];
+                if (typeof MAP_ON_PAGE !== 'undefined' && MAP_ON_PAGE) {
+                    document.write('<' + 'script src="//maps.google.com/maps/api/js?sensor=false"' + ' type="text/javascript"><' + '/script>');
+                }
+            </script>
+
+            <!--            <script type="text/javascript" src="//www.dur.ac.uk/js/scripts.min.js"></script>-->
+            <noscript>
+                <iframe src="//www.googletagmanager.com/ns.html?id=GTM-W9Q3S4"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            </noscript>
+            <script>(function (w, d, s, l, i) {
+                    w[l] = w[l] || [];
+                    w[l].push({
+                        'gtm.start':
+                            new Date().getTime(), event: 'gtm.js'
+                    });
+                    var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true;
+                    j.src =
+                        '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-W9Q3S4');</script>
+        </div>
+    </div>
+</div>
+</body>
+</html>
