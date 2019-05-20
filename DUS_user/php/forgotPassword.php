51711039bd0270a7d946d3f1161062daeee5055a
emma chen <emmachen@student-10-245-139-36.wlan.dur.ac.uk>
2019-05-21 00:09:21 +0100

    Update forgotPassword.php

--- DUS_user/php/forgotPassword.php
+++ DUS_user/php/forgotPassword.php
@@ -82,6 +82,7 @@
       </div>
     </div>
    </div>
+
    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
    <script type="text/javascript">
     var email = $("#email");
