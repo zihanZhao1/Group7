36b1bdf9f1eb1168be88125d6bcc9faf20a03d62
zihanZhao1 <50427581+zihanZhao1@users.noreply.github.com>
2019-05-23 07:38:38 +0800

    update

--- sei_course.sql
+++ sei_course.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 22, 2019 at 05:31 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- 主机： 127.0.0.1:3306
+-- 生成日期： 2019-05-22 22:59:57
+-- 服务器版本： 5.7.24
+-- PHP 版本： 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,52 +19,38 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `xsei`
+-- 数据库： `xsei`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sei_course`
+-- 表的结构 `sei_course`
 --
 
-CREATE TABLE `sei_course` (
-  `C_ID` int(11) NOT NULL,
-  `name` text COLLATE utf8_bin NOT NULL,
+DROP TABLE IF EXISTS `sei_course`;
+CREATE TABLE IF NOT EXISTS `sei_course` (
+  `C_ID` int(11) NOT NULL AUTO_INCREMENT,
   `F_ID` int(11) NOT NULL,
+  `name` text COLLATE utf8_bin NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
-  `week` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `week` int(11) NOT NULL,
+  PRIMARY KEY (`C_ID`),
+  KEY `F_ID` (`F_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `sei_course`
---
-
-INSERT INTO `sei_course` (`C_ID`, `name`, `F_ID`, `start_date`, `end_date`, `start_time`, `end_time`, `week`) VALUES
-(10002, 'Basket', 1124, '2019-06-01', '2019-06-28', '12:00:00', '14:00:00', 4);
-
---
--- Indexes for dumped tables
+-- 限制导出的表
 --
 
 --
--- Indexes for table `sei_course`
+-- 限制表 `sei_course`
 --
 ALTER TABLE `sei_course`
-  ADD PRIMARY KEY (`C_ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `sei_course`
---
-ALTER TABLE `sei_course`
-  MODIFY `C_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10003;
+  ADD CONSTRAINT `sei_course_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `sei_facility` (`F_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
