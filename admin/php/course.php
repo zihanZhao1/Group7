ebad28b0bc3f2e432fbf1981cdd1d429d4d42eeb
Ce Ma <50143960+MeccaBeast@users.noreply.github.com>
2019-05-22 23:34:12 +0800

    Add files via upload

--- admin/php/course.php
+++ admin/php/course.php
@@ -31,9 +31,12 @@
                 </td>
                 <td>CourseID</td>
                 <td>Name</td>
-                <td>Time</td>
-                <td>Weeks</td>
-                <td>Start</td>
+                <td>Facility ID</td>
+                <td>Start Date</td>
+                <td>End Date</td>
+                <td>Start Time</td>
+                <td>End Time</td>
+                <td>Week(Which weekday:1-7 means Mon-Sun)</td>
                 <td>Edit</td>
             </tr>
 
@@ -47,9 +50,12 @@
                 echo "<td><input type='checkbox' name='checkboxes[]' value='{$row['C_ID']}' />" . "</td>";
                 echo "<td>" . $row["C_ID"] . "</td>";
                 echo "<td>" . $row["name"] . "</td>";
-                echo "<td>" . $row["time"] . "</td>";
-                echo "<td>" . $row["weeks"] . "</td>";
-                echo "<td>" . $row["start"] . "</td>";
+                echo "<td>" . $row["F_ID"] . "</td>";
+                echo "<td>" . $row["start_date"] . "</td>";
+                echo "<td>" . $row["end_date"] . "</td>";
+                echo "<td>" . $row["start_time"] . "</td>";
+                echo "<td>" . $row["end_time"] . "</td>";
+                echo "<td>" . $row["week"] . "</td>";
                 echo "<td> <button type='button' class='btn_edit' name='edit' data-target='#addUserModal' data-toggle='modal' id={$row['C_ID']}>Edit</button ></td>";
             }
             ?>
@@ -81,21 +87,55 @@
                 <div class="modal-body">
                     <form class="form-horizontal" role="form">
                         <div class="form-group">
-                            <label> Course Name</label>
-                            <input type="text" name="name" id="name" class="form-control" required="required"/>
+                            <label style="margin-left: 20px;">Course Name</label>
+                            <input type="text" name="name" id="name" class="form-control" required="required" style= "width: 114px; border-left-width: 1px; margin-left: 20px;"/>
                         </div>
                         <div class="form-group">
-                            <label> Time</label>
-                            <input type="text" name="time" id="time" class="form-control" required="required"/>
-                        </div>
+                            <label style="margin-left: 20px;">Select Facility</label>
+                            <?php
+                            $result = $pdo->query("select F_ID, name from sei_facility");
 
-                        <div class="form-group">
-                            <label> Weeks</label>
-                            <input type="number" name="weeks" id="weeks" class="form-control" required="required"/>
+                            echo " <select name='f' id='f' style='margin-left: 20px;'>";
+
+                            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                                unset($fid, $fname,$rn);
+                                $fid = $row['F_ID'];
+                                $fname = $row['name'];
+                                echo '<option style="display:none;"></option>';
+                                echo '<option value="'.$fid.'">'.$fid.','.$fname.' </option>';
+                            }
+                            echo "</select>";
+                            ?><br>
+                            <span id="error_f" class="text-danger"></span>
                         </div>
                         <div class="form-group">
-                            <label> Start</label>
-                            <input type="date" name="start" id="start" class="form-control" required="required"/>
+                            <label style="margin-left: 20px;">Start Date</label>
+                            <input type="date" name="sdate" id="sdate" class="form-control" required="required" style= "width: 114px; border-left-width: 1px; margin-left: 20px;" min="<?=date('Y-m-d', strtotime('+1 days'))?>" max="<?=date('Y-m-d', strtotime('+180 days')) ?>"/>
+                        </div>
+                        <div class="form-group">
+                            <label style="margin-left: 20px;">End Date</label>
+                            <input type="date" name="edate" id="edate" class="form-control" required="required" style= "width: 114px; border-left-width: 1px; margin-left: 20px;" min="<?=date('Y-m-d', strtotime('+1 days'))?>" max="<?=date('Y-m-d', strtotime('+180 days')) ?>"/>
+                        </div>
+                        <div class="form-group">
+                            <label style="margin-left: 20px;">Enter Specific Week Day </label>
+                            <select name="week" id="week" style="margin-left: 20px;">
+                                <option style="display:none;"></option>
+                                <option value="1">Mon</option>
+                                <option value="2">Tue</option>
+                                <option value="3">Wed</option>
+                                <option value="4">Thu</option>
+                                <option value="5">Fri</option>
+                                <option value="6">Sat</option>
+                                <option value="7">Sun</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label style="margin-left: 20px;">Start Time</label>
+                            <input type="time" name="stime" id="stime" class="form-control" required="required" style= "width: 114px; border-left-width: 1px; margin-left: 20px;"/>
+                        </div>
+                        <div class="form-group">
+                            <label style="margin-left: 20px;">End Date</label>
+                            <input type="time" name="etime" id="etime" class="form-control" required="required" style= "width: 114px; border-left-width: 1px; margin-left: 20px;"/>
                         </div>
                         <div class="form-group" id="btn_content">
                             <input type="submit" name="form_action" id="form_action" class="btn btn-primary"
@@ -111,24 +151,31 @@
                     e.preventDefault();
                     var title = 'Add New Course';
                     $(".modal-title").html(title);
-                    var btn_insert = '<input type="submit" name="form_action" id="form_action" class="btn btn-primary" value="insert"/>';
+                    var btn_insert = '<input type="submit" name="form_action" id="form_action" class="btn btn-primary" value="Insert"  style="margin-left: 20px;"/>';
                     $("#btn_content").html(btn_insert);
+                    var nameID = $(this).attr("id");
                     $("#name").val("");
-                    $("#time").val("");
-                    $("#weeks").val("");
-                    $("#start").val("");
+                    $("#fid").val("");
+                    $("#sdate").val("");
+                    $("#edate").val("");
+                    $("#week").val("");
+                    $("#stime").val("");
+                    $("#etime").val("");
                     $("#form_action").on('click', function (e) {
                         e.preventDefault();
                         //the button got clicked
                         var name = document.getElementById("name").value;
-                        var time = document.getElementById("time").value;
-                        var weeks = document.getElementById("weeks").value;
-                        var start = document.getElementById("start").value;
+                        var fid = document.getElementById("fid").value;
+                        var sdate = document.getElementById("sdate").value;
+                        var edate = document.getElementById("edate").value;
+                        var week = document.getElementById("week").value;
+                        var stime = document.getElementById("stime").value;
+                        var etime = document.getElementById("etime").value;
                         var operate = "addCourse";
                         $.ajax({
                             url: "c_action.php",
                             type: 'post',
-                            data: {'operate': operate, 'name': name, 'time': time, 'weeks': weeks, 'start': start},
+                            data: {'operate': operate, 'name': name, 'fid': fid, 'sdate': sdate, 'edate': edate, 'week': week, 'stime': stime, 'etime': etime, 'nameID':nameID},
                             success: function got_success_msg(msg) {
                                 alert(msg);
                                 window.location.href = "course.php?";
@@ -143,7 +190,7 @@
                     e.preventDefault();
                     var title = 'Edit Course';
                     $(".modal-title").html(title);
-                    var btn_edit = '<input type="submit" name="form_action" id="form_action_edit" class="btn btn-primary" value="edit"/>';
+                    var btn_edit = '<input type="submit" name="form_action" id="form_action_edit" class="btn btn-primary" value="Edit"  style="margin-left: 20px;"/>';
                     $("#btn_content").html(btn_edit);
                     var nameID = $(this).attr("id");
                     var operate = "showCourse";
@@ -153,16 +200,22 @@
                         data: {'operate': operate, 'nameID': nameID},
                         success: function got_success_msg(msg) {
                             $("#name").val(msg.name);
-                            $("#time").val(msg.time);
-                            $("#weeks").val(msg.weeks);
-                            $("#start").val(msg.start);
+                            $("#fid").val(msg.fid);
+                            $("#sdate").val(msg.sdate);
+                            $("#edate").val(msg.edate);
+                            $("#week").val(msg.week);
+                            $("#stime").val(msg.stime);
+                            $("#etime").val(msg.etime);
                             $("#form_action_edit").on('click', function (e) {
                                 e.preventDefault();
                                 //the button got clicked
                                 var name = document.getElementById("name").value;
-                                var time = document.getElementById("time").value;
-                                var weeks = document.getElementById("weeks").value;
-                                var start = document.getElementById("start").value;
+                                var fid = document.getElementById("fid").value;
+                                var sdate = document.getElementById("sdate").value;
+                                var edate = document.getElementById("edate").value;
+                                var week = document.getElementById("week").value;
+                                var stime = document.getElementById("stime").value;
+                                var etime = document.getElementById("etime").value;
                                 var operate = "editCourse";
                                 $.ajax({
                                     url: "c_action.php",
@@ -170,9 +223,12 @@
                                     data: {
                                         'operate': operate,
                                         'name': name,
-                                        'time': time,
-                                        'weeks': weeks,
-                                        'start': start,
+                                        'fid': fid, 
+                                        'sdate': sdate,
+                                        'edate': edate, 
+                                        'week':week,
+                                        'stime': stime, 
+                                        'etime': etime,
                                         'nameID': nameID
                                     },
                                     success: function got_success_msg(msg) {
