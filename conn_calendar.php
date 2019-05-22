93f282d456dcf9864f750daa4a54ce0461d5d905
xinyi524 <50417949+xinyi524@users.noreply.github.com>
2019-05-22 10:21:47 +0100

    Add files via upload

---
+++ conn_calendar.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2019/5/11
+ * Time: 19:32
+ */
+
+include 'conn.php';
+session_start();
+$Uid = $_SESSION['userId'];
+//$Uid = 1;
+
+$tmpBooking = array();
+
+$Fid = $_SESSION['facility'];
+
+if ($Fid != 0)
+    $booking = $pdo->query("select * from sei_booking where U_ID = $Uid and F_ID=$Fid;");
+else {
+    $booking = $pdo->query("select * from sei_booking where U_ID = $Uid;");
+}
+
+
+foreach ($booking as $b) {
+    $Fid = $b['F_ID'];
+    $Fsql = "select * from sei_facility where F_ID = $Fid;";
+    $fac = $pdo->query($Fsql)->fetch(PDO::FETCH_ASSOC);
+
+    $b['title'] = $fac['name'] . ' num: ' . $b['count'];
+    array_push($tmpBooking, $b);
+}
+
+$tmpBooking = json_encode($tmpBooking);
+
+echo "var allBooking = $tmpBooking";
+?>
