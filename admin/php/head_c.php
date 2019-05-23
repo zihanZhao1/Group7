d045745abf7b11a3605559dd1b3692f96229eaa3
zihanZhao1 <50427581+zihanZhao1@users.noreply.github.com>
2019-05-24 07:47:37 +0800

    Add files via upload

---
+++ admin/php/head_c.php
@@ -0,0 +1,48 @@
+
+<body  class="home">
+<!--  -->
+<!--  -->		<div class="container-fluid">
+    <div id="header" class="row-fluid">
+        <div class="span12">
+            <h1>
+                <a href="/" class="pull-right">
+                    <img width="140" src="../image/durham-university-logo-white.png" alt="Durham University" class="durham-university-logo">
+                </a>
+                <a href="/" class="pull-left">
+                    <img src="../image/teamdurham.png" alt="Team Durham" class="team-durham-logo" />
+                </a>
+                <a href="/" class="team-durham-slogan">
+                    <span class="light">Durham University</span><br/>Sport<br/><br/>
+                    <span class="slogan">Enabling Exceptional People to do Exceptional Things</span>
+                </a>
+            </h1>
+        </div>
+    </div>		<div id="navigation" class="row-fluid">
+        <div class="span12">
+		<a href="logout.php" class="button" style="float:right;">logout</a>
+            <ul class="nav nav-pills">
+                <li><a href="facility.php">Facilities</a></li>
+                <li><a href="course.php">Courses</a></li>
+                <li><a href="calendar_All.php">Calendar</a></li>
+                <li><a href="booking.php">Bookings</a></li>
+                <li><a href="news.php">News</a></li>
+				
+            </ul>
+			
+        </div>
+    </div>		<!--<div id="content" class="row-fluid">
+    <div class="span3 pages">
+        <ul>
+            <li class='navcurrent'><a  href="#6"> My Details</strong></li>
+            <li class='sideways'><a  href="#7" >My bookings</a></li>
+    </div>
+</div> -->
+
+    <!--
+<div id="sponsor" class="row-fluid">
+<div class="span12">
+    Team Durham kindly sponsored by
+    [currently unspecified]
+</div>
+</div>
+-->	
