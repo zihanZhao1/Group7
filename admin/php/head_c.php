21c2241042e242a245586731f2a8d81f9d4237f8
zihanZhao1 <50427581+zihanZhao1@users.noreply.github.com>
2019-05-24 06:52:46 +0800

    admin v8

---
+++ admin/php/head_c.php
@@ -0,0 +1,50 @@
+	
+    <div id="header" class="row-fluid">
+        <div class="span12">
+            <h1>
+                <a href="/" class="pull-right">
+                    <img width="140" src="../img/durham-university-logo-white.png" alt="Durham University"
+                         class="durham-university-logo">
+                </a>
+                <a href="/" class="pull-left">
+                    <img src="../img/teamdurham.png" alt="Team Durham" class="team-durham-logo"/>
+                </a>
+                <a href="/" class="team-durham-slogan">
+                    <span class="light">Durham University</span><br/>Sport<br/><br/>
+                    <span class="slogan">Enabling Exceptional People to do Exceptional Things</span>
+                </a>
+            </h1>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      
+       
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" ">
+		
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="facility.php">Facilities<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="course.php">Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="calendar_All.php">Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+				 <li class="nav-item">
+                    <a class="nav-link" href="news.php">News</a>
+                </li>
+				
+            </ul>
+            <form method="post" action="search.php" onsubmit="return checkSearch();" class="form-inline my-2 my-lg-0">
+            
+
+                
+            </form>
+        </div>
+		<a href="logout.php" class="button" style="float:right;">logout</a>
+    </nav>
---
+++ admin/php/news.php
@@ -0,0 +1,198 @@
+<?php	
+	session_start();
+	$role = $_SESSION["role"];
+	if ($role == "admin") {
+		
+}else{
+  ?>
+   <script>
+
+    alert('You are not admin！');
+
+	window.location.href="../../user/php/index.php";
+   </script>
+<?php
+}
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/validate.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+    <form action="n_action.php" method="post">
+        <table>
+            <tr>
+                <td><input type="checkbox" name="checkboxes[]" value="on" onclick="selectAll(this,'checkboxes')"/>SelectAll
+                </td>
+                <td>NewsID</td>
+                <td>Title</td>
+                <td>Time</td>
+                <td>Content</td>
+                <td>Edit</td>
+            </tr>
+
+            <?php
+            require_once('database.php');
+            require_once('head.php');
+            $res = $pdo->query("select * from sei_news");
+            foreach ($res as $row) {
+                echo "<tr>";
+                echo "<td><input type='checkbox' name='checkboxes[]' value='{$row['N_ID']}' />" . "</td>";
+                echo "<td>" . $row["N_ID"] . "</td>";
+                echo "<td>" . $row["title"] . "</td>";
+                echo "<td>" . $row["time"] . "</td>";
+                echo "<td>" . $row["content"] . "</td>";
+                echo "<td> <button type='button' class='btn_edit' name='edit' data-target='#addUserModal' data-toggle='modal' id={$row['N_ID']}>Edit</button ></td>";
+            }
+            ?>
+        </table>
+        <input class="button" type="submit" name="delete_mul" value="Delete"/>
+    </form>
+    <div style="margin-top:1px;">
+        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addUserModal" name="add" id="add">
+            Add News
+        </button>
+        <div id="course_data" class="table-responsive">
+
+        </div>
+    </div>
+</div>
+<form method="post" action="" class="form-horizontal" role="form" id="form_data" onsubmit="return validate_form_news(this)"
+      style="margin: 30px;">
+    <div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" id="modal-dialog" title="Add News">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        &times;
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Add News
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label> Title</label>
+                            <input type="text" name="title" id="title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label> Time</label>
+                            <input type="date" name="time" id="time" class="form-control"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label> Content</label>
+                            <input type="text" name="content" id="content" class="form-control"/>
+                        </div>
+                        <div class="form-group" id="btn_content">
+                            <input type="submit" name="form_action" id="form_action" class="btn btn-primary"
+                                   value="insert"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script>
+            $(document).ready(function () {
+                $("#add").on('click', function (e) {
+                    e.preventDefault();
+                    var title = 'Add News';
+                    $(".modal-title").html(title);
+                    var btn_insert = '<input type="submit" name="form_action" id="form_action" class="btn btn-primary" value="insert"/>';
+                    $("#btn_content").html(btn_insert);
+                    $("#title").val("");
+                    $("#time").val("");
+                    $("#content").val("");
+                    $("#form_action").on('click', function (e) {
+                        e.preventDefault();
+                        //the button got clicked
+                        var title = document.getElementById("title").value;
+                        var time = document.getElementById("time").value;
+                        var content = document.getElementById("content").value;
+                        var operate = "addNews";
+                        $.ajax({
+                            url: "n_action.php",
+                            type: 'post',
+                            data: {'operate': operate, 'title': title, 'time': time, 'content': content},
+                            success: function got_success_msg(msg) {
+                                alert(msg);
+                                window.location.href = "news.php?";
+                            },
+                            dataType: "text"
+                        });
+                    });
+                });
+            });
+            $(document).ready(function () {
+                $(".btn_edit").on('click', function (e) {
+                    e.preventDefault();
+                    var title = 'Edit News';
+                    $(".modal-title").html(title);
+                    var btn_edit = '<input type="submit" name="form_action" id="form_action_edit" class="btn btn-primary" value="edit"/>';
+                    $("#btn_content").html(btn_edit);
+                    var nameID = $(this).attr("id");
+                    var operate = "showNews";
+                    $.ajax({
+                        url: "n_action.php",
+                        type: 'post',
+                        data: {'operate': operate, 'nameID': nameID},
+                        success: function got_success_msg(msg) {
+                            $("#title").val(msg.title);
+                            $("#time").val(msg.time);
+                            $("#content").val(msg.content);
+                            $("#form_action_edit").on('click', function (e) {
+                                e.preventDefault();
+                                //the button got clicked
+                                var title = document.getElementById("title").value;
+                                var time = document.getElementById("time").value;
+                                var content = document.getElementById("content").value;
+                                var operate = "editNews";
+                                $.ajax({
+                                    url: "n_action.php",
+                                    type: 'post',
+                                    data: {
+                                        'operate': operate,
+                                        'title': title,
+                                        'time': time,
+                                        'content': content,
+                                        'nameID': nameID
+                                    },
+                                    success: function got_success_msg(msg) {
+                                        alert(msg);
+                                        window.location.href = "news.php?";
+                                    },
+                                    dataType: "text"
+                                });
+                            });
+                        },
+                        dataType: "json"
+                    });
+                });
+            });
+
+
+        </script>
+        <script>
+            function selectAll(obj, chk) {
+                if (chk == null) {
+                    chk = 'checkboxes';
+                }
+                var elems = obj.form.getElementsByTagName("INPUT");
+                for (var i = 0; i < elems.length; i++) {
+                    if (elems[i].name == chk || elems[i].name == chk + "[]") {
+                        elems[i].checked = obj.checked;
+                    }
+                }
+            }
+        </script>
+
+    </div>
+</form>
+</body>
+</html>
